9807507c236bb66174e48e8a8fe2156e32713b10
Stephen Dickinson <stephencottontail@me.com>
2025-12-31 22:07:25 -0700

    `M-x customize` puts stuff in file not managed by Nix

--- init.el
+++ init.el
@@ -1,19 +1,16 @@
 ;;; -*- lexical-binding: t -*-
+(setq user-emacs-directory
+      (expand-file-name "emacs" (or (getenv "XDG_CONFIG_HOME")
+				    "~/.config")))
+
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+
+(let ((local-file (expand-file-name "custom.el" user-emacs-directory)))
+  (when (file-exists-p local-file)
+    (load local-file nil (quote nomessage))))
+      
 (load-theme (quote modus-vivendi-tinted) t)
 (add-hook (quote after-make-frame-functions)
           (lambda (frame)
             (with-selected-frame frame
               (set-frame-font "UnixWare" t t))))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
